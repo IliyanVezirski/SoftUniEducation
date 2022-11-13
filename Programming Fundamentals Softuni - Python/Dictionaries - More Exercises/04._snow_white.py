a8b06902908e18e76e8a1202d3dc1729dd50b821
IliyanVezirski <ilian.vezirski@gmail.com>
2022-11-13 15:28:19 +0200

    New_tasks

--- Programming Fundamentals Softuni - Python/Dictionaries - More Exercises/04._snow_white.py
+++ Programming Fundamentals Softuni - Python/Dictionaries - More Exercises/04._snow_white.py
@@ -1,32 +1,21 @@
-dwarfs = {
-    'names': [],
-    'hat': [],
-    'physics': []
-}
+dwarfs = {}
 
+counter_1 = 0
 data = input()
 while data != "Once upon a time":
     data = data.split(" <:> ")
     name = data[0]
     hat_color = data[1]
     physics = int(data[2])
-    if name not in dwarfs['names']:
-        dwarfs['names'].append(name)
-        dwarfs['hat'].append(hat_color)
-        dwarfs['physics'].append(physics)
-    elif name in dwarfs['names']:
-        index_for_current_dwarf = dwarfs['names'].index(name)
-        if dwarfs['hat'][index_for_current_dwarf] != hat_color:
-            dwarfs['names'].append(name)
-            dwarfs['hat'].append(hat_color)
-            dwarfs['physics'].append(physics)
-        else:
-            if physics > dwarfs['physics'][index_for_current_dwarf]:
-                dwarfs['physics'][index_for_current_dwarf] = physics
+    counter_1 += 1
+    founded_dwarf = False
+    for counter, dwarf in dwarfs.items():
+        if dwarfs[counter][0] == name and dwarfs[counter][1] == hat_color:
+            founded_dwarf = True
+            dwarfs[counter] = [name, hat_color, physics]
+    if not founded_dwarf:
+        dwarfs[counter_1] = [name, hat_color, physics]
     data = input()
-index_dict = {}
-for i, v in enumerate(dwarfs['names']):
-    index_dict[i] = v
-dwarfs['physics'] = sorted(dwarfs['physics'], key=lambda x: -x)
-print(index_dict)
-print(dwarfs)
+
+dwarfs = dict(sorted(dwarfs.items(), key=lambda kvp: (-kvp[1][2], dwarfs[1].count(kvp[1][1]))))
+[print(f"({dwarf[1]}) {dwarf[0]} <-> {dwarf[2]}") for count, dwarf in dwarfs.items()]
