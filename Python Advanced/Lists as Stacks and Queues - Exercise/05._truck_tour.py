750c36a343ba9a1d88dc29d12e02fee30f097993
Iliyan_Vezirski <115879384+IliyanVezirski@users.noreply.github.com>
2022-11-20 10:41:42 +0200

    new tasks

--- Python Advanced/Lists as Stacks and Queues - Exercise/05._truck_tour.py
+++ Python Advanced/Lists as Stacks and Queues - Exercise/05._truck_tour.py
@@ -1,19 +1,24 @@
 from collections import deque
 
-petrol_pumps = int(input())
-distance = deque()
-petrol_filled = deque()
+queue = deque()
+number_of_gas_station = int(input())
 
-for i in range(petrol_pumps):
-    pump = input()
-    pump = [int(i) for i in pump.split()]
-    petrol_filled.append(pump[0])
-    distance.append(pump[1])
-index = 0
-while True:
-    current_petrol = petrol_filled.popleft()
-    current_distance = distance.popleft()
-    if current_petrol > current_distance:
+for i in range(number_of_gas_station):
+    gas_pump = input().split()
+    queue.append([int(gas_pump[0]), int(gas_pump[1])])
+
+for index_gas in range(len(queue)):
+    fuel = 0
+    current_pump_ok = True
+    for gas_stats in queue:
+        current_fuel, current_distance = gas_stats[0], gas_stats[1]
+        fuel += current_fuel
+        if fuel >= current_distance:
+            fuel -= current_distance
+        else:
+            current_pump_ok = False
+            break
+    if current_pump_ok:
+        print(index_gas)
         break
-    index += 1
-print(index)
+    queue.rotate(-1)
