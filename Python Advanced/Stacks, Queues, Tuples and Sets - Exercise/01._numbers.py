f47874795550ad68fc9004a415766a6ced35a61c
IliyanVezirski <ilian.vezirski@gmail.com>
2023-01-19 15:50:30 +0200

    New tasks

--- Python Advanced/Stacks, Queues, Tuples and Sets - Exercise/01._numbers.py
+++ Python Advanced/Stacks, Queues, Tuples and Sets - Exercise/01._numbers.py
@@ -1,20 +1,20 @@
-def add_first(first_set: set, set_to_add: set):
-    [first_set.add(i) for i in set_to_add]
+def add_first(first_set: set, set_to_add: list):
+    [first_set.add(int(i)) for i in set_to_add]
     return first_set
 
 
-def add_second(second_set: set, set_to_add: set):
-    [second_set.add(i) for i in set_to_add]
+def add_second(second_set: set, set_to_add: list):
+    [second_set.add(int(i)) for i in set_to_add]
     return second_set
 
 
-def remove_first(first_set: set, set_to_remove: set):
-    [first_set.discard(i) for i in set_to_remove]
+def remove_first(first_set: set, set_to_remove: list):
+    [first_set.discard(int(i)) for i in set_to_remove]
     return first_set
 
 
-def remove_second(second_set: set, set_to_remove: set):
-    [second_set.discard(i) for i in set_to_remove]
+def remove_second(second_set: set, set_to_remove: list):
+    [second_set.discard(int(i)) for i in set_to_remove]
     return second_set
 
 
@@ -24,8 +24,8 @@ def check_subset(first_set: set, second_set: set):
     return False
 
 
-first_sequence = set(input().split())
-second_sequence = set(input().split())
+first_sequence = set(map(int, input().split()))
+second_sequence = set(map(int, input().split()))
 
 number_of_command = int(input())
 
@@ -33,15 +33,15 @@ for _ in range(number_of_command):
     command = input().split()
     if command[0] == "Add":
         if command[1] == "First":
-            add_first(first_sequence, set(command[2:]))
+            add_first(first_sequence, command[2:])
         else:
-            add_second(second_sequence, set(command[2:]))
+            add_second(second_sequence, command[2:])
     elif command[0] == "Remove":
         if command[1] == "First":
-            remove_first(first_sequence, set(command[2:]))
+            remove_first(first_sequence, command[2:])
         else:
-            remove_second(second_sequence, set(command[2:]))
-    else:
+            remove_second(second_sequence, command[2:])
+    elif command[0] == "Check":
         print(check_subset(first_sequence, second_sequence))
 print(*list(sorted(first_sequence, key=lambda x: x)), sep=', ')
 print(*list(sorted(second_sequence, key=lambda x: x)), sep=', ')
