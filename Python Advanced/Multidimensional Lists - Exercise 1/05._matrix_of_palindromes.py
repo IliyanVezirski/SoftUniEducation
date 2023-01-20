0f61f58d0b619742ab2e7bdbafaaf7b589a3df03
IliyanVezirski <ilian.vezirski@gmail.com>
2023-01-20 19:59:17 +0200

    new tasks

--- Python Advanced/Multidimensional Lists - Exercise 1/05._matrix_of_palindromes.py
+++ Python Advanced/Multidimensional Lists - Exercise 1/05._matrix_of_palindromes.py
@@ -2,7 +2,7 @@ start_point = ord('a')
 
 raws, col = [int(i) for i in input().split()]
 matrix = []
-[matrix.append([(f"{chr(start_point + r)}{chr(start_point + r + c)}{chr(start_point + r)}") for c in range(col)]) for r
+[matrix.append([(chr(start_point + r) + chr(start_point + r + c) + chr(start_point + r)) for c in range(col)]) for r
  in
  range(raws)]
 [print(*matrix[r], sep=' ') for r in range(len(matrix))]
