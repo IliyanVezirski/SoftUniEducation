7201acd768ca745ba3bc7e840954d2010faf43af
IliyanVezirski <ilian.vezirski@gmail.com>
2023-01-21 12:12:37 +0200

    new tasks

--- Python Advanced/Multidimensional Lists - Exercise 2/01._flatten_lists.py
+++ Python Advanced/Multidimensional Lists - Exercise 2/01._flatten_lists.py
@@ -1,4 +1,6 @@
 first_part = input().split('|')
-second_part = [[i for i in first_part[r].split()] for r in range(len(first_part) - 1, -1, -1)]
-for r in second_part:
-    print(*r,end=' ')
+result = []
+for r in range(len(first_part) - 1, -1, -1):
+    current = first_part[r].split()
+    result += current
+print(*result)
