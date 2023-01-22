d6c1888aff5cadc4ca0e764e6898381967c3e008
IliyanVezirski <ilian.vezirski@gmail.com>
2023-01-22 16:12:53 +0200

    new tasks

--- Python Advanced/Stacks, Queues, Tuples and Sets - Exercise/04._honey.py
+++ Python Advanced/Stacks, Queues, Tuples and Sets - Exercise/04._honey.py
@@ -4,24 +4,29 @@ working_bees = deque(int(i) for i in input().split())
 nectar = [int(i) for i in input().split()]
 honey_making_process = deque(input().split())
 result = 0
-while True:
-    if not nectar or not working_bees:
-        break
-    current_bee = working_bees.popleft()
-    current_nectar = nectar.pop()
-    while current_nectar < current_bee:
-        current_nectar = nectar.pop()
-        if not nectar:
-            break
-    to_do = honey_making_process.popleft()
-    if to_do == "-":
-        result += abs(current_bee - current_nectar)
-    elif to_do == '+':
-        result += abs(current_bee + current_nectar)
-    elif to_do == "/":
-        result += abs(current_bee / current_nectar)
-    elif to_do == "*":
-        result += abs(current_bee * current_nectar)
+while working_bees and nectar:
+    current_bee = working_bees[0]
+    current_nectar = nectar[-1]
+    if current_nectar >= current_bee:
+        if working_bees:
+            working_bees.popleft()
+        if nectar:
+            nectar.pop()
+        to_do = honey_making_process.popleft()
+        if to_do == "+":
+            result += abs(current_bee + current_nectar)
+        elif to_do == "-":
+            result += abs(current_bee - current_nectar)
+        elif to_do == '/':
+            if current_bee != 0 and current_nectar != 0:
+                result += abs(current_bee / current_nectar)
+        elif to_do == '*':
+            result += abs(current_bee * current_nectar)
+
+    else:
+        if nectar:
+            nectar.pop()
+
 print(f'Total honey made: {result}')
 if nectar:
     print(f"Nectar left: {', '.join(list(map(str, nectar)))}")
