337e13e190ad3eea4c924ea0a6c409ab9ab5bf4c
IliyanVezirski <ilian.vezirski@gmail.com>
2023-01-22 15:57:24 +0200

    new tasks

--- Python Advanced/Stacks, Queues, Tuples and Sets - Exercise/06._paint_colors.py
+++ Python Advanced/Stacks, Queues, Tuples and Sets - Exercise/06._paint_colors.py
@@ -1,95 +1,54 @@
 from collections import deque
-import math
+
 string = deque(input().split())
 
-substring = string[0] + string[-1]
-main_colors = []
-secondary_colors = []
-indexes_of_secondary_colors = []
-
-
-def pop_from_string(string: deque):
-    if len(string) > 1:
-        first = string.pop()
-        second = string.popleft()
-    else:
-        string.pop()
-
-
-def color_append(substring):
-    if substring in "redyellowblue":
-        main_colors.append(substring)
-        pop_from_string(string)
-        return True
-    elif substring in "orangepurplegreen":
-        if substring == "orange":
-            if 'red' in main_colors and 'yellow' in main_colors:
-                main_colors.append(substring)
-                pop_from_string(string)
-                return True
-            else:
-                secondary_colors.append(substring)
-                indexes_of_secondary_colors.append(len(main_colors))
-                pop_from_string(string)
-                return True
-        elif substring == "purple":
-            if 'red' in main_colors and 'blue' in main_colors:
-                main_colors.append(substring)
-                pop_from_string(string)
-                return True
-            else:
-                secondary_colors.append(substring)
-                indexes_of_secondary_colors.append(len(main_colors))
-                pop_from_string(string)
-                return True
-        elif substring == "green":
-            if 'yellow' in main_colors and 'blue' in main_colors:
-                main_colors.append(substring)
-                pop_from_string(string)
-                return True
-            else:
-                secondary_colors.append(substring)
-                indexes_of_secondary_colors.append(len(main_colors))
-                pop_from_string(string)
-                return True
-        else:
-            pop_from_string(string)
-            return True
-    else:
-        return False
-
+colors = []
+MAIN_COLORS = ["red", "yellow", "blue"]
+SECONDARY_COLORS = ["orange", "purple", "green"]
 
 while string:
     if len(string) > 1:
-        substring = string[-1] + string[0]
-        substring2 = string[0] + string[-1]
+        first_substring = string[-1] + string[0]
+        second_substring = string[0] + string[-1]
+        if first_substring in MAIN_COLORS or first_substring in SECONDARY_COLORS:
+            colors.append(first_substring)
+            string.pop()
+            string.popleft()
+        elif second_substring in MAIN_COLORS or second_substring in SECONDARY_COLORS:
+            colors.append(second_substring)
+            string.pop()
+            string.popleft()
+        else:
+            first_substring = string[0][:-1]
+            second_substring = string[-1][:-1]
+            string.pop()
+            string.popleft()
+            if first_substring:
+                string.insert(len(string) // 2, first_substring)
+            if second_substring:
+                string.insert(len(string) // 2, second_substring)
+
     else:
         substring = string[0]
-        substring2 = substring
-    if color_append(substring):
-        continue
-    elif color_append(substring2):
-        continue
-    else:
-        if len(string) > 1:
-            first = string.pop()
-            first = first[:len(first) - 1]
-            second = string.popleft()
-            second = second[:len(second) - 1]
-            string.insert(int(math.ceil(len(string) / 2)), first)
-            string.insert(int(math.ceil(len(string) / 2)), second)
-            string = deque(i for i in string if i)
+        if substring in MAIN_COLORS or substring in SECONDARY_COLORS:
+            colors.append(substring)
+            string.pop()
         else:
-            color_append(string[0])
-            break
-for index in range(len(secondary_colors)):
-    if secondary_colors[index] == "orange":
-        if 'red' in main_colors and 'yellow' in main_colors:
-            main_colors.insert(indexes_of_secondary_colors[index], secondary_colors[index])
-    elif secondary_colors[index] == "purple":
-        if 'red' in main_colors and 'blue' in main_colors:
-            main_colors.insert(indexes_of_secondary_colors[index], secondary_colors[index])
-    elif secondary_colors[index] == 'green':
-        if 'yellow' in main_colors and 'blue' in main_colors:
-            main_colors.insert(indexes_of_secondary_colors[index], secondary_colors[index])
-print(main_colors)
+            string.pop()
+for color in colors:
+    if color == "orange":
+        if "red" in colors and "yellow" in colors:
+            continue
+        else:
+            colors.remove(color)
+    if color == "purple":
+        if 'red' in colors and 'blue' in colors:
+            continue
+        else:
+            colors.remove(color)
+    if color == "green":
+        if 'yellow' in colors and 'blue' in colors:
+            continue
+        else:
+            colors.remove(color)
+print(colors)
