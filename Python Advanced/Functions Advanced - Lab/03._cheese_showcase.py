6c8d8c54c310b650c16fc86a384bef1083a04e1a
IliyanVezirski <ilian.vezirski@gmail.com>
2023-01-23 09:51:20 +0200

    New tasks

--- Python Advanced/Functions Advanced - Lab/03._cheese_showcase.py
+++ Python Advanced/Functions Advanced - Lab/03._cheese_showcase.py
@@ -1,7 +1,7 @@
 def sorting_cheeses(**kwargs):
+    sorted_dict = sorted(kwargs.items(), key=lambda x: (-len(x[1]), x[0]))
     result = ''
-    for name, date in kwargs.items():
-        result += name + '\n'
-        for n in date:
-            result += str(n) + '\n'
+    for name, date in sorted_dict:
+        sorted_date = list(sorted(date, reverse=True))
+        result += name + '\n' + '\n'.join(list(map(str, sorted_date))) + '\n'
     return result
