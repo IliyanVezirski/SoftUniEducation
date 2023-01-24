2a4a40fbf54289b524579194e916aaa2d9fbfd08
IliyanVezirski <ilian.vezirski@gmail.com>
2023-01-24 21:11:28 +0200

    new tasks

--- Python Advanced/Functions Advanced - Lab/05._operate.py
+++ Python Advanced/Functions Advanced - Lab/05._operate.py
@@ -1,8 +1,8 @@
 def recursive_power(number: int, power: int):
     if power == 0:
-        return number * number
-    return recursive_power(number, power - 1)
+        return 1
+    return recursive_power(number, power - 1) * number
 
-print(recursive_power(2, 10))
+print(recursive_power(3, 10))
 
-print(2 ** 10)
+print(3 ** 10)
