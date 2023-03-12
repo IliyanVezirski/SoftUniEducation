7bb1a9d0a31b345c80745e9957ef11de50439816
IliyanVezirski <ilian.vezirski@gmail.com>
2023-03-12 13:40:19 +0200

    new tasks

--- Python OOP/Encapsulation - Lab/03._profile.py
+++ Python OOP/Encapsulation - Lab/03._profile.py
@@ -1,29 +1,41 @@
 class Profile:
 
-    def __init__(self, username, password):
-        self.__username = set_username(username)
-        self.__password = password(password)
+    def __init__(self, username='', password=''):
+        self.username = username
+        self.password = password
 
-    def get_username(self):
-        return self.__username
+    @property
+    def username(self):
+        return self.username
 
-    def set_username(self, value):
-        if len(value) >= 5 and len(value) <= 15:
+    @username.setter
+    def username(self, value):
+        if 5 <= len(value) <= 15:
             self.__username = value
         else:
             raise ValueError("The username must be between 5 and 15 characters.")
 
+    @property
     def password(self):
-        return self.__password
+        return self.password
 
+    @password.setter
     def password(self, value):
-        upper_value = False
-        if len(value) >= 8:
+        if len(value) < 8:
+            raise ValueError("The password must be 8 or more characters with at least 1 digit and 1 uppercase letter.")
+        else:
+            upper_letter = False
+            digit_letter = False
             for letter in value:
                 if letter == letter.upper():
-                    upper_value = True
-        if upper_value:
-            self.__password = value
-        else:
-            raise ValueError("The password must be 8 or more characters with at least 1 digit and 1 uppercase letter.")
-profile_with_invalid_password = Profile('My_username', 'My-password')
+                    upper_letter = True
+                if letter.isdigit():
+                    digit_letter = True
+            if upper_letter and digit_letter:
+                self.__password = value
+            else:
+                raise ValueError(
+                    "The password must be 8 or more characters with at least 1 digit and 1 uppercase letter.")
+
+    def __str__(self):
+        return f"You have a profile with username: \"{self.__username}\" and password: {len(self.__password) * '*'}"
