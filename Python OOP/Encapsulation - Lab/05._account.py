5851dd9291c0d0973a37072fc6b52ee10b168ff5
IliyanVezirski <ilian.vezirski@gmail.com>
2023-03-12 19:11:21 +0200

    new tasks

---
+++ Python OOP/Encapsulation - Lab/05._account.py
@@ -0,0 +1,18 @@
+class Account:
+    def __init__(self, id, balance, pin):
+        self.__id = id
+        self.balance = balance
+        self.__pin = pin
+
+    def get_id(self, pin):
+        if self.__pin == pin:
+            return self.__id
+        else:
+            return f'Wrong pin'
+
+    def change_pin(self, old_pin, new_pin):
+        if self.__pin == old_pin:
+            self.__pin = new_pin
+            return f'Pin changed'
+        else:
+            return f'Wrong pin'
