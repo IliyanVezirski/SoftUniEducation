5c47730c6e85ca97093471a8b19a8487d30b627c
IliyanVezirski <ilian.vezirski@gmail.com>
2023-03-12 21:26:25 +0200

    new tasks

--- Python OOP/Encapsulation - Exercise/01._wild_cat_zoo/project/zoo.py
+++ Python OOP/Encapsulation - Exercise/01._wild_cat_zoo/project/zoo.py
@@ -12,14 +12,14 @@ class Zoo:
         self.animals = []
         self.workers = []
 
-    def add_animals(self, animal: Animal, price):
+    def add_animal(self, animal: Animal, price):
         if self.__budget >= price and self.__animal_capacity - len(self.animals) > 0:
             self.__budget -= price
             self.animals.append(animal)
             return f"{animal.name} the {animal.__class__.__name__} added to the zoo"
-        if self.__animal_capacity - len(self.animals) > 0:
-            return f'Not enough budget'
         if self.__budget < price:
+            return f'Not enough budget'
+        if self.__animal_capacity - len(self.animals) >= 0:
             return f'Not enough space for animal'
 
     def hire_worker(self, worker: Worker):
@@ -52,6 +52,7 @@ class Zoo:
         if current_tend <= self.__budget:
             self.__budget -= current_tend
             return f"You tended all the animals. They are happy. Budget left: {self.__budget}"
+        return "You have no budget to tend the animals. They are unhappy."
 
     def profit(self, amount):
         self.__budget += amount
@@ -69,46 +70,25 @@ class Zoo:
                 tigers.append(animal)
             elif animal.__class__.__name__ == 'Cheetah':
                 cheetah.append(animal)
+        result += f"----- {len(lions)} Lions:\n" + '\n'.join([str(i) for i in lions]) + '\n'
+        result += f"----- {len(tigers)} Tigers:\n" + '\n'.join([str(i) for i in tigers]) + '\n'
+        result += f"----- {len(cheetah)} Cheetahs:\n" + '\n'.join([str(i) for i in cheetah])
+        return result
 
-from project.caretaker import Caretaker
-from project.cheetah import Cheetah
-from project.keeper import Keeper
-from project.lion import Lion
-from project.tiger import Tiger
-from project.vet import Vet
-from project.zoo import Zoo
-
-
-zoo = Zoo("Zootopia", 3000, 5, 8)
-
-# Animals creation
-animals = [Cheetah("Cheeto", "Male", 2), Cheetah("Cheetia", "Female", 1), Lion("Simba", "Male", 4), Tiger("Zuba", "Male", 3), Tiger("Tigeria", "Female", 1), Lion("Nala", "Female", 4)]
-
-# Animal prices
-prices = [200, 190, 204, 156, 211, 140]
-
-# Workers creation
-workers = [Keeper("John", 26, 100), Keeper("Adam", 29, 80), Keeper("Anna", 31, 95), Caretaker("Bill", 21, 68), Caretaker("Marie", 32, 105), Caretaker("Stacy", 35, 140), Vet("Peter", 40, 300), Vet("Kasey", 37, 280), Vet("Sam", 29, 220)]
-
-# Adding all animals
-for i in range(len(animals)):
-    animal = animals[i]
-    price = prices[i]
-    print(zoo.add_animal(animal, price))
-
-# Adding all workers
-for worker in workers:
-    print(zoo.hire_worker(worker))
-
-# Tending animals
-print(zoo.tend_animals())
-
-# Paying keepers
-print(zoo.pay_workers())
-
-# Fireing worker
-print(zoo.fire_worker("Adam"))
-
-# Printing statuses
-print(zoo.animals_status())
-print(zoo.workers_status())
+    def workers_status(self):
+        result = ''
+        result += f"You have {len(self.workers)} workers\n"
+        keeper = []
+        caretaker = []
+        vets = []
+        for worker in self.workers:
+            if worker.__class__.__name__ == 'Keeper':
+                keeper.append(worker)
+            elif worker.__class__.__name__ == 'Caretaker':
+                caretaker.append(worker)
+            elif worker.__class__.__name__ == 'Vet':
+                vets.append(worker)
+        result += f"----- {len(keeper)} Keepers:\n" + '\n'.join([str(i) for i in keeper]) + '\n'
+        result += f"----- {len(caretaker)} Caretakers:\n" + '\n'.join([str(i) for i in caretaker]) + '\n'
+        result += f"----- {len(vets)} Vets:\n" + '\n'.join([str(i) for i in vets])
+        return result
